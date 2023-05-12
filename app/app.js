b874becb5bcb6c17ba520d26ffb767caa282fbd0
Samuel <samyvelezm@gmail.com>
2023-05-12 17:35:07 -0500

    v1 agregando usuarios

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ import routeHome from './routes/backoffice.routes.js';
 import route from "./routes/home.routes.js"
 import dash from './routes/dashboard.routes.js';
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(express.static(__dirname + '../public'));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // TODAS LAS RUTAS Y PERMISOS DE GOOGLE
 app.use("/auth", passport.authenticate("auth-google", {
--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -1,6 +1,7 @@
-import { Router } from "express";
+import { Router, response } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
 
@@ -27,15 +28,32 @@ dash.get("/inicio", (req, res) => {
 });
 
 // Ruta vista usuarios
-dash.get("/usuario", (req, res) => {
+dash.get("/usuario", async(req, res) => {
     if (req.cookies.ckmp){
         try {
             const token = jwt.verify(req.cookies.ckmp, process.env.SECRET_KEY);
+
+            let ruta = "http://localhost:3000/api/user";
+            let option = {
+                method: "GET"
+            };
+
+            let datos = {};
+
+            const result = await fetch(ruta, option)
             
+            .then(response => response.json())
+            .then(data => {
+                datos = data[0];
+                // console.log(data[0]);
+            })
+            .catch(error => console.error("Este es el error: " + error))
+
             res.render("dash", {
                 "nombre": token.nombre,
                 "foto": token.foto,
-                "menu": 1
+                "menu": 1,
+                "datos": datos
             });
 
         } catch (error) {
@@ -83,6 +101,40 @@ dash.get("/categoria", (req, res) => {
         }
     } else {
         res.redirect("/");
+    }
+});
+
+//Insertar informacion desde formulario
+dash.post("/guardar", (req, res) => {
+    if(req.body.name){
+
+        let ruta = "http://localhost:3000/api/user";
+        let metodo = "post";
+
+
+        let option = {
+            method: metodo,
+            Headers: {
+                "Content-Type": "application/json"
+            },
+            data:{
+                name: req.body.name
+            }
+        };
+
+        try {
+            const result = fetch(ruta, option)
+            .then(res => res.json())
+            .then(data => {
+
+            })
+            .catch(err => console.log("Error al consumir API: " + err))
+        } catch (error) {
+            
+        }
+
+    }else{
+        console.send("Este es el error: " );
     }
 });
 
