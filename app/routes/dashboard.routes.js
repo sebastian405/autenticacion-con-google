c341fb0da0adb81b19102ba6048c2e99fa7cbba7
Samuel <samyvelezm@gmail.com>
2023-05-19 17:38:55 -0500

    boton de editar hecho

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -2,7 +2,6 @@ import { Router, response } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
-import { strategies } from "passport";
 
 dotenv.config();
 
@@ -109,14 +108,23 @@ dash.get("/categoria", (req, res) => {
 dash.post("/guardar", (req, res) => {
     if(req.body.name){
 
-        let data = {name: req.body.name};
-        let ruta = "http://localhost:3000/api/user";
+        let data = {
+            name: req.body.name
+        };
         let metodo = "post";
 
+        if(req.body.id){
+            data = {
+                id: req.body.id,
+                name: req.body.name
+            };
+            metodo = "put"
+        }
 
+        let ruta = "http://localhost:3000/api/user";
         let option = {
             method: metodo,
-            Headers: {
+            headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
@@ -138,6 +146,32 @@ dash.post("/guardar", (req, res) => {
         console.send("Este es el error: " );
     }
 });
+
+dash.get("/edit-user", (req, res) => {
+    const id = req.query.id;
+    const name = req.query.name;
+
+    let datos = {
+        id: id,
+        name: name
+    }
+
+    if (req.cookies.ckmp){
+        try {
+            const token = jwt.verify(req.cookies.ckmp, process.env.SECRET_KEY);
+
+            res.render("dash", {
+                "nombre": token.nombre,
+                "foto": token.foto,
+                "menu": 4,
+                "datos": datos
+            });
+
+        } catch(error){
+            console.error("error con el token" + error);
+        }
+    }
+})
 
 // Ruta para salir del dash
 dash.get("/salir", (req, res) => {
